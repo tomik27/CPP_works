6e080b089a86328ee62e0d5beac3c92c6fd35aaf
tstom <tstom@DESKTOP-G3DSATN>
2019-11-13 11:12:08 +0100

    cv6

--- work_6/main.cpp
+++ work_6/main.cpp
@@ -4,14 +4,13 @@
 #include <string>
 using namespace std;
 
-void uloz(Osoba* pole,int pocet) {
+void uloz(Osoba* pole,int pocet,string soubor) {
 	ofstream out{};
-	out.open("pokus.txt");
+	out.open(soubor);
 	for (size_t i = 0; i < pocet; i++)
 	{
 		out << pole[i];
 	}
-	delete[]pole;
 	out.close();
 }
 Osoba* nacti(int pocet){
@@ -25,52 +24,70 @@ Osoba* nacti(int pocet){
 	}
 	return pole;
 }
-//binanrni neni hotovy
-//void ulozBin() {
-//	ifstream f{ "soubor.dat",ios_base::out | ios_base::binary };
-//	Osoba osoba{ 120,150,30 };
-//	f.write(osoba.getJmeno(), osoba.getJmeno().length()+1);
-//	
-//	//write string
-//	string s{ osoba.getJmeno() };
-//	s.resize(100);
-//	f.write(s.c_str(), s.size());
-//	f.close();
-//}
-//Osoba* nactiBin() {
-//	ifstream binarniSoubor{ "soubor.dat",ios_base::in | ios_base::binary };
-//	Osoba* pole = new Osoba[3];
-//	for (size_t i = 0; i < 3; i++)
-//	{
-//		//jedine po znacich
-//		string s{};
-//		char znak;
-//		while (znak = f.get()) {
-//			s += znak;
-//		}
-//		//string
-//		char retezec[100];
-//		f.read(retezec, sizeof retezec);
-//		string s.{retezec, 100};
-//		cout << "Nacteno: " << s << endl;
-//		cin.get();
-//
-//	}
-//	binarniSoubor.close();
-//
-//}
+void methodWriteString(string text, ostream& f) {
+	string s{ text };
+	s.resize(100);
+	f.write(s.c_str(), s.size());
+}
+void ulozBn(Osoba* pole, int pocet) {
+	ofstream f{ "soubor.dat",ios_base::out | ios_base::binary };
+	for (size_t i = 0; i < pocet; i++)
+	{
+		/*string s{ pole[i].getJmeno() };
+	s.resize(100);
+	f.write(s.c_str(), s.size());*/
+		methodWriteString(pole[i].getJmeno(),f);
+		methodWriteString(pole[i].getPrijmeni(), f);
+		methodWriteString(pole[i].getTrvaleBydliste().getMesto(), f);
+		methodWriteString(pole[i].getTrvaleBydliste().getPSC(), f);
+		methodWriteString(pole[i].getTrvaleBydliste().getUlice(), f);
+		methodWriteString(to_string(pole[i].getDatumNarozeni().getDen()), f);
+		methodWriteString(to_string(pole[i].getDatumNarozeni().getMesic()), f);
+		methodWriteString(to_string(pole[i].getDatumNarozeni().getRok()), f);
+	}
+	f.close();
+}
+string methodReadString(istream& f) {
+	char retezec[100];
+f.read(retezec, sizeof retezec);
+string s{retezec, 100};
+return s;
+}
+Osoba* nactiBn(int pocet) {
+		ifstream f{ "soubor.dat",ios_base::in | ios_base::binary };
+	Osoba* pole = new Osoba[pocet];
+	for (size_t i = 0; i < pocet; i++)
+	{
+
+		string jmeno = methodReadString(f);
+		string prijmeni = methodReadString(f);
+		string ulice = methodReadString(f);
+		string mesto = methodReadString(f);
+		string psc = methodReadString(f); 
+		int den = stoi(methodReadString(f));
+		int mesic= stoi(methodReadString(f));
+        int rok= stoi(methodReadString(f));
+		//string prijmeni = methodReadString(f);
+		//cin.get();
+		Osoba osoba(jmeno, prijmeni, Adresa(ulice, mesto, psc), Datum(den, mesic, rok));
+		pole[i] = osoba;
+	}
+	f.close();
+	return pole;
+}
+
 int main() {
 	Osoba osoba1("Tomas", "Nikdo", Adresa("ulice","Mesto","psc"), Datum(1,1,1));
 	Osoba osoba2("Ondra", "Nikdo", Adresa("ulice", "Mesto", "psc"), Datum(1, 1, 1));
 	Osoba osoba3("Pa5ey", "Nikdo", Adresa("ulice", "Mesto", "psc"), Datum(1, 1, 1));
 	Osoba* pole = new Osoba[3];
 
-	cout << osoba1;
 	pole[0] = osoba1;
 	pole[1] = osoba2;
 	pole[2] = osoba3;
-	uloz(pole,3);
-	Osoba* pole1 = nacti(3);
-
+	//uloz(pole,3, "pokus.txt");
+	//Osoba* pole1 = nacti(3);
+	ulozBn(pole, 3);
+	Osoba* pole1=nactiBn(3);
 	return 0;
 }
